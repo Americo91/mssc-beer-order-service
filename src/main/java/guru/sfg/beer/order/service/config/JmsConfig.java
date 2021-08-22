4ec70b01db2e0e19988596b09b12c5242bc9f60b
americo stoppello <americo.stoppello@gmail.com>
2021-08-22 11:05:55 +0200

    JMS messaging

---
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -0,0 +1,27 @@
+package guru.sfg.beer.order.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by @author stopp on 10/08/2021
+ */
+@Configuration
+public class JmsConfig {
+
+    public static final String MY_QUEUE = "my-hello-world";
+    public static final String MY_SEND_RCV_QUEUE = "replybacktome";
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+
+}
